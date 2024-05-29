8546ddf60926abcbbef6554a846bb9a69be1978b
anurag singh <102454228+anu6569201@users.noreply.github.com>
2024-05-29 10:55:13 +0530

    updated vercel json and build sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-set -o errexit
+#!/bin/bash
 
-# Build the project
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install django
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
